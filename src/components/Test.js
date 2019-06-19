1082742795bfcbed471e3a9a7281a3d82bcec753
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-06-18 18:26:43 -0600

    Now keeps track of high score and can reset score.

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,21 +3,34 @@ import React from "react";
 class Test extends React.Component {
   // Initial state
   state = {
-    score: 0
+    score: 0,
+    highScore: 0
   };
 
-  // handleIncrement increments score by 1
+  // Increments score by 1
   handleIncrement = () => {
-    this.setState({ score: this.state.score + 1 });
+    if (this.state.score >= this.state.highScore) {
+      this.setState({ score: this.state.score + 1, highScore: this.state.highScore + 1 });
+    } else {
+      this.setState({ score: this.state.score + 1 });
+    }
   };
+
+  handleReset = () => {
+    this.setState({ score: 0 });
+  }
 
   render() {
     return (
 
       <div>
         <p>Score: {this.state.score}</p>
+        <p>High Score: {this.state.highScore}</p>
         <button className="btn btn-primary" onClick={this.handleIncrement}>
           +1
+          </button>
+        <button className="btn btn-secondary" onClick={this.handleReset}>
+          Reset
           </button>
       </div>
 
